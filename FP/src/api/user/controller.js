64e1b729d2c64a82fa98a7f00202c9402552b8d1
dudckd6744 <dudckd6744@naver.com>
2022-01-22 22:22:57 +0900

    FP awilix-koa 설치

--- FP/src/api/user/controller.js
+++ FP/src/api/user/controller.js
@@ -1,26 +1,35 @@
 const Router = require("koa-router");
+const { createController } = require('awilix-koa')
 
-const router = new Router();
+// function UserController() {
+//   //   ttete();
+//   return function ttete() {
+//     const route = new Router();
+//     const path = "/user";
 
-function UserController() {
-  //   ttete();
-  return function ttete() {
-    const route = new Router();
-    const path = "/user";
+//     route.get("/").get("/test", test1).get("/test2");
 
-    route.get("/").get("/test", test1).get("/test2");
+//     router.use(path, route.routes());
 
-    router.use(path, route.routes());
+//     function test1() {
+//       return console.log("test2");
+//     }
+//   };
+//   //   const test = (ctx) => {
+//   //     console.log("G");
+//   //     ctx.body = "ok";
+//   //   };
+// }
 
-    function test1() {
-      return console.log("test2");
-    }
-  };
-  //   const test = (ctx) => {
-  //     console.log("G");
-  //     ctx.body = "ok";
-  //   };
-}
-const test = new UserController();
-console.log(test);
-module.exports = { UserController };
+const API = ({ todoService }) => ({
+  getTodo: async ctx => {(ctx.body = await todoService.get("ctx.params.id")),console.log("sds")},
+  createTodo: async ctx =>
+    (ctx.body = await todoService.create(ctx.request.body))
+})
+
+module.exports = createController(API)
+
+  .prefix('/user') // Prefix all endpoints with `/todo`
+  .get('/', 'getTodo') // Maps `GET /todos/:id` to the `getTodo` function on the returned object from `API`
+
+// module.exports={createController}
--- FP/src/index.js
+++ FP/src/index.js
@@ -1,22 +1,32 @@
 const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
-const Router = require("koa-router");
+const { asClass, createContainer} = require("awilix")
+const {loadControllers, scopePerRequest, createController} = require("awilix-koa")
+// const Router = require("koa-router");
 
 const app = new koa();
-const router = new Router();
+// const router = new Router();
 function createApp(controllers) {
+
+
+const container = createContainer().register({
+  // userService: asClass(/*...*/"tet"),
+  todoService: asClass(createController)
+})
+app.use(scopePerRequest(container))
   app.use(bodyParser());
-  app.use(router.routes());
+  // app.use(router.routes());
+  app.use(loadControllers('/api/**/*.{ts,js}', { cwd: __dirname }))
 
   app.listen(2000, () => console.log(`listening to port ${2000}`));
 
-  const route = new Router();
-  console.log(controllers);
-  controllers.forEach((router) => {
-    console.log(router);
-    route.use(router);
-  });
-  router.use("/api", route.routes());
+  // const route = new Router();
+
+  // controllers.forEach((router) => {
+  //   console.log(router);
+  //   route.use(router);
+  // });
+  // router.use("/api", route.routes());
 }
 
 module.exports = { createApp };
